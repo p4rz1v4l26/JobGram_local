a06845e0bf2b0979b441bacc00090e59bf703ae1
p4rz1v4l26 <20211cst0067@gmail.com>
2024-03-24 09:49:12 +0530

    Error Handling set

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/userRouter.js";
 import applicationRouter from "./routes/applicationRouter.js";
 import jobRouter from "./routes/jobRouter.js";
 import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 const app = express();
 
@@ -35,10 +36,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/application", applicationRouter);
 app.use("/api/v1/job", jobRouter);
 
-
 dbConnection();
 
-
-
-
+app.use(errorMiddleware);
 export default app;
