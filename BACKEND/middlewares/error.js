eecb86042970748810de89b7a6601522aefb67a0
p4rz1v4l26 <20211cst0067@gmail.com>
2024-03-29 14:14:57 +0530

    bugs fix

--- BACKEND/middlewares/error.js
+++ BACKEND/middlewares/error.js
@@ -4,27 +4,28 @@ class ErrorHandler extends Error {
     this.statusCode = statusCode;
   }
 }
-export const errorMiddleware = (err, req, res) => {
-  err.message = err.message || "Internal Server error";
+
+export const errorMiddleware = (err, req, res, next) => {
+  err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
-  if (err.name === "CaseError") {
-    const message = `Resource not found. Invalid  ${err.path}`;
-    err = new ErrorHandler(message, 400);
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid ${err.path}`,
+      err = new ErrorHandler(message, 400);
   }
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
-    err = new ErrorHandler(message, 400);
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
+      err = new ErrorHandler(message, 400);
   }
   if (err.name === "JsonWebTokenError") {
-    const message = `Json Web Token is Invalid, Try Again`;
+    const message = `Json Web Token is invalid, Try again!`;
     err = new ErrorHandler(message, 400);
   }
   if (err.name === "TokenExpiredError") {
-    const message = `Json Web Token is Expired. Reset it and try again later .`;
+    const message = `Json Web Token is expired, Try again!`;
     err = new ErrorHandler(message, 400);
   }
-  return res.stats(err.statusCode).json({
+  return res.status(err.statusCode).json({
     success: false,
     message: err.message,
   });
