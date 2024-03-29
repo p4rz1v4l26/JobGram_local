f517592f7ac9b13ef376e7b0171897d334b13b67
p4rz1v4l26 <20211cst0067@gmail.com>
2024-03-29 22:29:13 +0530

    User auto done T_T

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -1,7 +1,7 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/userSchema.js";
 import ErrorHandler from "../middlewares/error.js";
-// import { sendToken } from "../utils/jwtToken.js";
+import { sendToken } from "../utils/jwtToken.js";
 
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, phone, role, password } = req.body;
@@ -20,50 +20,48 @@ export const register = catchAsyncError(async (req, res, next) => {
     password,
   });
 
+  sendToken(user, 201, res, "User Registered!");
+});
 
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password, role } = req.body;
+  if (!email || !password || !role) {
+    return next(new ErrorHandler("Please provide email ,password and role."));
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email Or Password.", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email Or Password.", 400));
+  }
+  if (user.role !== role) {
+    return next(
+      new ErrorHandler(`User with provided email and ${role} not found!`, 404)
+    );
+  }
+  sendToken(user, 201, res, "User Logged In!");
+});
 
-//   sendToken(user, 201, res, "User Registered!");
-// });
-
-// export const login = catchAsyncError(async (req, res, next) => {
-//   const { email, password, role } = req.body;
-//   if (!email || !password || !role) {
-//     return next(new ErrorHandler("Please provide email ,password and role."));
-//   }
-//   const user = await User.findOne({ email }).select("+password");
-//   if (!user) {
-//     return next(new ErrorHandler("Invalid Email Or Password.", 400));
-//   }
-//   const isPasswordMatched = await user.comparePassword(password);
-//   if (!isPasswordMatched) {
-//     return next(new ErrorHandler("Invalid Email Or Password.", 400));
-//   }
-//   if (user.role !== role) {
-//     return next(
-//       new ErrorHandler(`User with provided email and ${role} not found!`, 404)
-//     );
-//   }
-//   sendToken(user, 201, res, "User Logged In!");
-// });
-
-// export const logout = catchAsyncError(async (req, res, next) => {
-//   res
-//     .status(201)
-//     .cookie("token", "", {
-//       httpOnly: true,
-//       expires: new Date(Date.now()),
-//     })
-//     .json({
-//       success: true,
-//       message: "Logged Out Successfully.",
-//     });
-// });
+export const logout = catchAsyncError(async (req, res, next) => {
+  res
+    .status(201)
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Logged Out Successfully.",
+    });
+});
 
 // export const getUser = catchAsyncError((req, res, next) => {
 //   const user = req.user;
-  res.status(200).json({
-    success: true,
-    message: "User Registered successfully",
-    user,
-  });
-});
+//   res.status(200).json({
+//     success: true,
+//     message: "User Registered successfully",
+//     user,
+//   });
+// });
