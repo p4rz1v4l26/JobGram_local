de31f732cdded808d3ab9169b03ac4e8cc533dcf
p4rz1v4l26 <20211cst0067@gmail.com>
2024-03-30 18:26:50 +0530

    minor grammar update

--- BACKEND/models/jobSchema.js
+++ BACKEND/models/jobSchema.js
@@ -9,7 +9,7 @@ const jobSchema = new mongoose.Schema({
   },
   description: {
     type: String,
-    required: [true, "Please provide decription."],
+    required: [true, "Please provide description."],
     minLength: [30, "Description must contain at least 30 Characters!"],
     maxLength: [500, "Description cannot exceed 500 Characters!"],
   },
@@ -28,7 +28,7 @@ const jobSchema = new mongoose.Schema({
   location: {
     type: String,
     required: [true, "Please provide location."],
-    minLength: [20, "Location must contian at least 20 characters!"],
+    minLength: [20, "Location must contain at least 20 characters!"],
   },
   fixedSalary: {
     type: Number,
