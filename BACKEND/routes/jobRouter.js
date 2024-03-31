9785c5c034549a097d8c37ab7cab4c1e9eaec52b
p4rz1v4l26 <20211cst0067@gmail.com>
2024-03-31 15:24:59 +0530

    Router updates

--- BACKEND/routes/jobRouter.js
+++ BACKEND/routes/jobRouter.js
@@ -1,4 +1,17 @@
 import express from "express";
+import {
+  getAllJobs,
+  getMyJobs,
+  postJob,
+  updateJob,
+} from "../controllers/jobController.js";
+import { isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+router.get("/getAll", getAllJobs);
+router.post("/post", isAuthorized, postJob);
+router.get("/getmyjobs", isAuthorized, getMyJobs);
+router.put("/update/:id", isAuthorized, updateJob);
+
 export default router;
