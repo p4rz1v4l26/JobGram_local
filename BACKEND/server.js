e3933052d2903d269a105636f85e3a8cd9048b97
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-03 23:06:50 +0530

    bugs fixed

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
