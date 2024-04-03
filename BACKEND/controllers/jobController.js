d04b86a1beb913f297f9b623e4e54b91f8dce4eb
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-03 14:26:47 +0530

    Controllers Update

--- BACKEND/controllers/jobController.js
+++ BACKEND/controllers/jobController.js
@@ -123,7 +123,7 @@ export const deleteJob = catchAsyncError(async (req, res, next) => {
   });
 });
 
-export const getSingleJob = catchAsyncErrors(async (req, res, next) => {
+export const getSingleJob = catchAsyncError(async (req, res, next) => {
   const { id } = req.params;
   try {
     const job = await Job.findById(id);
