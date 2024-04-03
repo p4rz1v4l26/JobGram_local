abe8e1718722920055e4ded46be0f06cfc736562
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-03 23:45:22 +0530

    Router updates

--- BACKEND/routes/userRouter.js
+++ BACKEND/routes/userRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, register, logout } from "../controllers/userController.js";
+import {
+  login,
+  register,
+  logout,
+  getUser,
+} from "../controllers/userController.js";
 import { isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -7,4 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthorized, logout);
+router.get("/getUser", isAuthorized, getUser);
+
 export default router;
