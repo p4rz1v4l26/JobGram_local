9d09129ea06fb7f62a511bea27d9c280bcbcaf6d
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-04 22:12:25 +0530

    Basic setup

---
+++ FRONTEND/src/components/Auth/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const login = () => {
+  return <></>;
+};
+
+export default login;
---
+++ FRONTEND/src/components/Home/PopularCompanies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PopularCompanies = () => {
+  return <></>;
+};
+
+export default PopularCompanies;
