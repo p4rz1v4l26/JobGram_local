0dfc50b2d7eed6d37c93ad0153f38144c976cd61
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-05 18:04:28 +0530

    Footer added

--- FRONTEND/src/components/Layout/Footer.jsx
+++ FRONTEND/src/components/Layout/Footer.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaTwitter, FaGithub, FaDiscord } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
-  return <></>;
+  const { isAuthorized } = useContext(Context);
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By P4RZ1V4L26.</div>
+      <div>
+        <Link
+          to={
+            "https://in.linkedin.com/in/avinash-warale-098654233?trk=public_post_follow-view-profile/u"
+          }
+          target="_blank"
+        >
+          <FaLinkedin />
+        </Link>
+        <Link to={"https://twitter.com/avinashwarale/"} target="_blank">
+          <FaTwitter />
+        </Link>
+        <Link to={"https://github.com/P4RZ1V4L26"} target="_blank">
+          <FaGithub />
+        </Link>
+        <Link
+          to={"https://discordapp.com/users/896411007797325824/"}
+          target="_blank"
+        >
+          <FaDiscord />
+        </Link>
+        <Link to={"https://www.instagram.com/26__avinash/"} target="_blank">
+          <RiInstagramFill />
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
