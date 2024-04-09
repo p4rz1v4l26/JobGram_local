2ccd9c4131eee51a875f1d996b749b33deec9058
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-09 19:35:48 +0530

    To get all jobs

--- FRONTEND/src/components/Job/MyJobs.jsx
+++ FRONTEND/src/components/Job/MyJobs.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { FaCheck } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+import { Context } from "../../main";
+import { useNavigate } from "react-router-dom";
 
 const MyJobs = () => {
-  return <></>;
-};
+  const [myJobs, setMyJobs] = useState([]);
+  const [editingMode, setEditingMode] = useState(null);
+  const { isAuthorized, user } = useContext(Context);
 
-export default MyJobs;
+  const navigateTo = useNavigate();
+  //Fetching all jobs
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/job/getmyjobs",
+          { withCredentials: true }
+        );
+        setMyJobs(data.myJobs);
+      } catch (error) {
+        toast.error(error.response.data.message);
+        setMyJobs([]);
+      }
+    };
+    fetchJobs();
+  }, []);
+  if (!isAuthorized || (user && user.role !== "Employer")) {
+    navigateTo("/");
+  }
+
+  const handleEnableEdit = (jobId) => {
+    setEditingMode(jobId);
+  };
+
+  const handleDisableEdit = () => {
+    setEditingMode(null);
+  };
