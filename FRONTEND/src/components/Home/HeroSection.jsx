18994e5f038684d3254bd4c8e07eda7fc34811bc
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-09 12:58:30 +0530

    Hero Section added

--- FRONTEND/src/components/Home/HeroSection.jsx
+++ FRONTEND/src/components/Home/HeroSection.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { FaBuilding, FaSuitcase, FaUsers, FaUserPlus } from "react-icons/fa";
 
 const HeroSection = () => {
-  return <></>;
+  const details = [
+    {
+      id: 1,
+      title: "2,34,568",
+      subTitle: "Live Job",
+      icon: <FaSuitcase />,
+    },
+    {
+      id: 2,
+      title: "69,420",
+      subTitle: "Companies",
+      icon: <FaBuilding />,
+    },
+    {
+      id: 3,
+      title: "3,45,670",
+      subTitle: "Job Seekers",
+      icon: <FaUsers />,
+    },
+    {
+      id: 4,
+      title: "5,05,621",
+      subTitle: "Employers",
+      icon: <FaUserPlus />,
+    },
+  ];
+  return (
+    <>
+      <div className="heroSection">
+        <div className="container">
+          <div className="title">
+            <h1>Find a job </h1>
+            <h1>your interests and skills</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              voluptate repellat modi quidem aliquid eaque ducimus ipsa et,
+              facere mollitia!
+            </p>
+          </div>
+          <div className="image">
+            <img src="/heroS.jpg" alt="hero" />
+          </div>
+        </div>
+        <div className="details">
+          {details.map((element) => {
+            return (
+              <div className="card" key={element.id}>
+                <div className="icon">{element.icon}</div>
+                <div className="content">
+                  <p>{element.title}</p>
+                  <p>{element.subTitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HeroSection;
