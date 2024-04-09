3b896195f1a2e333aa925472edf3e81fd44b428b
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-09 16:29:35 +0530

    Popular Categories Section created

--- FRONTEND/src/components/Home/PopularCategories.jsx
+++ FRONTEND/src/components/Home/PopularCategories.jsx
@@ -1,7 +1,84 @@
 import React from "react";
+import {
+  MdOutlineDesignServices,
+  MdOutlineWebhook,
+  MdAccountBalance,
+  MdOutlineAnimation,
+} from "react-icons/md";
+import { TbAppsFilled } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { GiArtificialIntelligence } from "react-icons/gi";
+import { IoGameController } from "react-icons/io5";
 
 const PopularCategories = () => {
-  return <></>;
+  const categories = [
+    {
+      id: 1,
+      title: "Graphics & Design",
+      subTitle: "406 Open Positions",
+      icon: <MdOutlineDesignServices />,
+    },
+    {
+      id: 2,
+      title: "Mobile App Development",
+      subTitle: "603 Open Positions",
+      icon: <TbAppsFilled />,
+    },
+    {
+      id: 3,
+      title: "Frontend Web Development",
+      subTitle: "270 Open Positions",
+      icon: <MdOutlineWebhook />,
+    },
+    {
+      id: 4,
+      title: "MERN STACK Development",
+      subTitle: "2000+ Open Postions",
+      icon: <FaReact />,
+    },
+    {
+      id: 5,
+      title: "Account & Finance",
+      subTitle: "350 Open Positions",
+      icon: <MdAccountBalance />,
+    },
+    {
+      id: 6,
+      title: "Artificial Intelligence",
+      subTitle: "950 Open Positions",
+      icon: <GiArtificialIntelligence />,
+    },
+    {
+      id: 7,
+      title: "Video Animation",
+      subTitle: "68 Open Positions",
+      icon: <MdOutlineAnimation />,
+    },
+    {
+      id: 8,
+      title: "Game Development",
+      subTitle: "93 Open Positions",
+      icon: <IoGameController />,
+    },
+  ];
+  return (
+    <div className="categories">
+      <h3>POPULAR CATEGORIES</h3>
+      <div className="banner">
+        {categories.map((element) => {
+          return (
+            <div className="card" key={element.id}>
+              <div className="icon">{element.icon}</div>
+              <div className="text">
+                <p>{element.title}</p>
+                <p>{element.subTitle}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PopularCategories;
