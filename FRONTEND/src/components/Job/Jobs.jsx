e1bbeb3a8c4fcf4197ace46a5ad7e488dd1b84ac
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-10 11:24:11 +0530

    Search bar added for All jobs

--- FRONTEND/src/components/Job/Jobs.jsx
+++ FRONTEND/src/components/Job/Jobs.jsx
@@ -5,41 +5,55 @@ import { Context } from "../../main";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
   const { isAuthorized } = useContext(Context);
   const navigateTo = useNavigate();
+
   useEffect(() => {
-    try {
-      axios
-        .get("http://localhost:4000/api/v1/job/getall", {
-          withCredentials: true,
-        })
-        .then((res) => {
-          setJobs(res.data);
-        });
-    } catch (error) {
-      console.log(error);
-    }
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/job/getall",
+          { withCredentials: true }
+        );
+        setJobs(response.data.jobs);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchJobs();
   }, []);
-  if (!isAuthorized) {
-    navigateTo("/");
-  }
+
+  useEffect(() => {
+    if (!isAuthorized) {
+      navigateTo("/");
+    }
+  }, [isAuthorized, navigateTo]);
+
+  const filteredJobs = jobs.filter((job) =>
+    job.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <section className="jobs page">
       <div className="container">
         <h1>ALL AVAILABLE JOBS</h1>
+        <input
+          type="text"
+          placeholder="Search jobs"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="search-bar" // Apply the class for search bar styling
+        />
         <div className="banner">
-          {jobs.jobs &&
-            jobs.jobs.map((element) => {
-              return (
-                <div className="card" key={element._id}>
-                  <p>{element.title}</p>
-                  <p>{element.category}</p>
-                  <p>{element.country}</p>
-                  <Link to={`/job/${element._id}`}>Job Details</Link>
-                </div>
-              );
-            })}
+          {filteredJobs.map((element) => (
+            <div className="card" key={element._id}>
+              <p>{element.title}</p>
+              <p>{element.category}</p>
+              <p>{element.country}</p>
+              <Link to={`/job/${element._id}`}>Job Details</Link>
+            </div>
+          ))}
         </div>
       </div>
     </section>
